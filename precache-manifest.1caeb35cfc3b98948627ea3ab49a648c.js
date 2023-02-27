1932592c85b3268f614e1810b9327ea773bb4734
monartlondon <monart.london@gmail.com>
2023-02-27 16:10:06 +0000

    Updates

---
+++ precache-manifest.1caeb35cfc3b98948627ea3ab49a648c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00ef65c9831cf457f9f8c8ef81626ee",
+    "url": "/Stillness/index.html"
+  },
+  {
+    "revision": "72e91285664145c15c86",
+    "url": "/Stillness/static/css/main.c568d7bd.chunk.css"
+  },
+  {
+    "revision": "4060d7652eb7d880285b",
+    "url": "/Stillness/static/js/2.87afc4d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Stillness/static/js/2.87afc4d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e91285664145c15c86",
+    "url": "/Stillness/static/js/main.db04b393.chunk.js"
+  },
+  {
+    "revision": "9529e2cf7e608cd8b011",
+    "url": "/Stillness/static/js/runtime-main.bf55da3b.js"
+  },
+  {
+    "revision": "3151ab15cbe2ae0895505f8d7fe77918",
+    "url": "/Stillness/static/media/about.3151ab15.jpg"
+  },
+  {
+    "revision": "da46702edc3739d72dcdb6c945c7c1c2",
+    "url": "/Stillness/static/media/blog1.da46702e.jpg"
+  },
+  {
+    "revision": "17fac3e21cb7ff9f23c1e4ca02ea4d8e",
+    "url": "/Stillness/static/media/blog2.17fac3e2.jpg"
+  },
+  {
+    "revision": "b7ec7a139dcda72b3bcad691933bd2ab",
+    "url": "/Stillness/static/media/blog3.b7ec7a13.jpg"
+  },
+  {
+    "revision": "73c8f49bd2b04df742c4302164c259a2",
+    "url": "/Stillness/static/media/blog4.73c8f49b.jpg"
+  },
+  {
+    "revision": "67eade3b8ed9fcb5c66f0fe71ffbde8e",
+    "url": "/Stillness/static/media/blog5.67eade3b.jpg"
+  },
+  {
+    "revision": "f8c16df489a2c3d5fd37e2f6c0097cd9",
+    "url": "/Stillness/static/media/campaign.f8c16df4.gif"
+  },
+  {
+    "revision": "c78f059024cd251c46dd8c404bc0635b",
+    "url": "/Stillness/static/media/footer1.c78f0590.jpg"
+  },
+  {
+    "revision": "ae1d745e9cc9e0812886776a4975bc77",
+    "url": "/Stillness/static/media/footer2.ae1d745e.jpg"
+  },
+  {
+    "revision": "02a5d48e8a9539f4e8bab14ae50d096f",
+    "url": "/Stillness/static/media/footer3.02a5d48e.jpg"
+  },
+  {
+    "revision": "1c859772231f49d44fb8a1eefc7d3329",
+    "url": "/Stillness/static/media/footer4.1c859772.jpg"
+  },
+  {
+    "revision": "fc6609b557f7a71f0b36b32fa2004db5",
+    "url": "/Stillness/static/media/footer5.fc6609b5.jpg"
+  },
+  {
+    "revision": "5a79c664054e2ad415fbe871e14ad0db",
+    "url": "/Stillness/static/media/img1.5a79c664.jpg"
+  },
+  {
+    "revision": "84469ec03c00a69ec9cfcd66bee8c37e",
+    "url": "/Stillness/static/media/img2.84469ec0.jpg"
+  },
+  {
+    "revision": "80bd2a00df590ec2d53210458deb31b1",
+    "url": "/Stillness/static/media/img3.80bd2a00.jpg"
+  },
+  {
+    "revision": "252021f03323282850ec3dd5ee0a9832",
+    "url": "/Stillness/static/media/img4.252021f0.jpg"
+  },
+  {
+    "revision": "73c8f49bd2b04df742c4302164c259a2",
+    "url": "/Stillness/static/media/main1.73c8f49b.jpg"
+  },
+  {
+    "revision": "ff3d9c52769d9ae1ed4fa9ab9e23829f",
+    "url": "/Stillness/static/media/main2.ff3d9c52.jpg"
+  },
+  {
+    "revision": "16af1b61473d5e5b83889a6ddd3c93b0",
+    "url": "/Stillness/static/media/main3.16af1b61.jpg"
+  },
+  {
+    "revision": "e491bd0047e811a1bfaaffbe6207a269",
+    "url": "/Stillness/static/media/main4.e491bd00.jpg"
+  }
+]);
--- src/components/Logo/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-const Logo = (props) => {
-  return <div className="logo">Stillness</div>;
-};
-
-export default Logo;
